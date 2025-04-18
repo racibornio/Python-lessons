e01fc42b8afd3b51d5851971ee6d44251f7d1a3a
Patryk Ostrowski <ostrowski.patryk@gmail.com>
2025-04-18 18:48:25 +0200

    Dokończenie list i dodanie krotek.

---
+++ struktury_danych/krotki.py
@@ -0,0 +1,7 @@
+#immutable, ordered, iterable, can be a key in dictionary
+#useful for returning multiple values from functions, keys in dictionaries
+
+empty_tuple = set()
+print('Empty tuple', empty_tuple)
+
+krotka1int = (0, 1, 2, 3)
