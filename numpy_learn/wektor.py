aeb7b2023b7564bb0d711ae213d0a670ad52a1c9
Patryk Ostrowski <ostrowski.patryk@gmail.com>
2025-04-20 09:25:31 +0200

    Dodanie wektora macierzy dwuwymiarowej.

--- numpy_learn/wektor.py
+++ numpy_learn/wektor.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+#zad.: utwórz wektor
+
 #utw. wektor przedstawiający wiersz
 vector_row = np.array([1, 2, 3])
 
@@ -10,3 +12,10 @@ vector_column = np.array([[1],
 
 print('Row shape', vector_row.shape)
 print('Column shape', vector_column.shape)
+
+vector_mixed = np.array([
+    [0, 1, 1],
+    [1, 2, 3],
+    [2, 3, 4]
+])
+print('Mixed vector', vector_mixed.shape)
