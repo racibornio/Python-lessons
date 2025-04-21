ea13863b8c85c2efb02e618ad5bbdacaa1ba6079
Patryk Ostrowski <ostrowski.patryk@gmail.com>
2025-04-21 14:45:53 +0200

    Odkomentowany program budujący listę z inputów od usera.

--- struktury_danych/listy.py
+++ struktury_danych/listy.py
@@ -127,25 +127,25 @@ for i in range(len(list_for_data)):
 
 print('List length:', len(list_for_data))
 
-# #wariant 1 - wypełnij listę podanymi wartościami
-# lista_do_wypelnienia = []
-# licznik = 10
-# for i in range(1, 11):
-#     lista_do_wypelnienia.append(input(f'Wpisz dane jeszcze {licznik} razy'))
-#     licznik -= 1
-#     print('Lista teraz:', lista_do_wypelnienia)
-#
-# print('Cała lista to', lista_do_wypelnienia)
-#
-#
-# #wariant 2 - wypełnij listę podanymi wartościami - szybsza
-# lista_inaczej = []
-# for i, e in enumerate(range(10, 0, -1)):
-#     lista_inaczej.append(input(f'Wpisz dane do listy jeszcze {e} razy'))
-#     print('Lista teraz:', lista_inaczej)
-#     #print('i:', i, 'e:', e)
-#
-# print('Cała lista:', lista_inaczej)
+#wariant 1 - wypełnij listę podanymi wartościami
+lista_do_wypelnienia = []
+licznik = 10
+for i in range(1, 11):
+    lista_do_wypelnienia.append(input(f'Wpisz dane jeszcze {licznik} razy'))
+    licznik -= 1
+    print('Lista teraz:', lista_do_wypelnienia)
+
+print('Cała lista to', lista_do_wypelnienia)
+
+
+#wariant 2 - wypełnij listę podanymi wartościami - szybsza
+lista_inaczej = []
+for i, e in enumerate(range(10, 0, -1)):
+    lista_inaczej.append(input(f'Wpisz dane do listy jeszcze {e} razy'))
+    print('Lista teraz:', lista_inaczej)
+    print('i:', i, 'e:', e)
+
+print('Cała lista:', lista_inaczej)
 
 
 #rozszerzanie listy
