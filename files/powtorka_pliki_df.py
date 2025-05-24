55e21b3fdc18e471bc8f767a285d6ebb1b395c70
Patryk <ostrowski.patryk@gmail.com>
2025-05-24 16:01:00 +0200

    Powtórka - c.d.

--- files/powtorka_pliki_df.py
+++ files/powtorka_pliki_df.py
@@ -1,3 +1,5 @@
+from traceback import print_tb
+
 import pandas as pd
 
 df = pd.read_csv('metody_df.csv')
@@ -33,16 +35,16 @@ print(type(lista_kolumn))
 
 print()
 df_length = len(df)
-print(f'Length data frame {df_length}')
+print(f'Długość df (liczba wierszy) {df_length}')
 print()
 
 
 df_kolumny = len(df.columns)
-print(f'Kolumny: {df_kolumny}')
+print(f'Szerokość df (liczba kolumn): {df_kolumny}')
 print()
 
 df_size = df.size
-print(f'Size df: {df_size}')
+print(f'Rozmiar/powierzchnia df (liczba komórek): {df_size}')
 print()
 
 df_shape = df.shape
@@ -51,4 +53,58 @@ df_shape_0 = df.shape[0]
 print(f'Shape[0]: {df_shape_0}')
 df_shape_1 = df.shape[1]
 print(f'Shape[1]: {df_shape_1}')
+print()
 
+sum_inty = df['inty'].sum()
+print(f'Sum inty: {sum_inty}')
+
+count_inty = df['inty'].count()
+print(f'Count inty: {count_inty}')
+print()
+
+sum_floaty = df['floaty'].sum()
+print(f'Sum floaty: {sum_floaty}')
+
+count_floaty = df['floaty'].count()
+print(f'Count floaty: {count_floaty}')
+print()
+
+sum_stringi = df['stringi'].sum()
+print(f'Sum stringi {sum_stringi}')
+
+count_stringi = df['stringi'].count()
+print(f'Count stringi: {count_stringi}')
+print()
+
+# sum_daty = df['daty'].sum()
+# print(f'Sum daty: {sum_daty}')
+
+count_daty = df['daty'].count() # zlicza wartości niepuste
+print(f'Count daty: {count_daty}')
+print()
+
+null_inty = df['inty'].isnull() # zwraca obiekt Series typu bool
+print(f'Nulle dla inty: {null_inty}')
+suma_null_inty = df['inty'].isnull().sum()
+print(f'Suma nulli dla inty: {suma_null_inty}')
+print()
+
+not_null_inty = df['inty'].notnull()
+print(f'Not null dla inty: {not_null_inty}')
+suma_not_null_inty = df['inty'].notnull().sum()
+print(f'Suma not null dla inty: {suma_not_null_inty}')
+print()
+
+for column in df.columns:
+    column_type = df[column].dtype
+    if column_type != 'object':
+        column_sum = df[column].sum()
+        print(f'Suma kolumny {column} wynosi: {column_sum}.')
+
+
+print()
+for column in df.columns:
+    column_count = df[column].count()
+    null_count = df[column].isnull().sum()
+    not_null_count = df[column].notnull().sum()
+    print(f'Kolumna {column} zawiera {column_count} wartości niepustych i {null_count} null i {not_null_count} not null.')
