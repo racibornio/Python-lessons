1201e3f33ceaf177e473f6e39e5e007c86ea8445
Patryk <ostrowski.patryk@gmail.com>
2026-08-25 18:47:00 +0200

    Freeze bibliotek do pliku requirements.txt

--- clean_offers.py
+++ clean_offers.py
@@ -2,8 +2,15 @@ import re
 
 # Wklej tutaj wieloliniowy tekst ogłoszenia w potrójnym cudzysłowie:
 raw_job_description = """
- 21 840 – 30 240 PLN
-+ VAT (B2B) per month  
+Cześć Patryku!
+Czy interesuje Cię rola Business Data Analityka? Widzę, że obecnie działasz jako PO, więc stąd moje pytanie jakie kierunki bierzesz pod uwagę :) 
+Zachęcam do zaaplikowania jeśli Cię interesuje temat analizy danych z biznesowym podejściem dla ekosystemu finansowego (są to spółki zajmujące się tematami paymentowami, finansowymi, troche krypto - projekt, który Daftcode zdecydował się rozwijać na ten moment troche bardziej niż stawianie nowych startupow :)) podrzucam link do formularza aplikacyjnego: https://daftcode.traffit.com/public/form/a/370?uid=17e82a21447bb0c4b2593e4de04eff364c62706e51584e6a4a6b6d58443775794a7642644d49786d30704a5044795a46
+oczywiscie w razie pytań pisz śmiało :)
+pzdr,
+Eliza
+
+Eliza Lesiak
+HR Generalist || IT Recruiter || Emotionally Intelligent People Partner || Extended DISC® Consultant || Making work more human  
 """
 
 
